d9de6dfc1188a12ce060207d8936db77120bdc39
sohey1910 <soheyi@163.com>
2020-07-14 14:21:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import tensorflow as tf
 from skimage import data, img_as_float
 
+from comput_ms_ssim import *
+
 image = data.camera()
 img = img_as_float(image)
 rows, cols = img.shape
